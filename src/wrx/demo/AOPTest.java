1d31c07b008e669ca67ccbbc0a1d383c9e2fda3b
wuruixiong <wowuruixiong@gmail.com>
2019-06-20 20:25:25 +0800

    add comment

--- src/wrx/demo/AOPTest.java
+++ src/wrx/demo/AOPTest.java
@@ -8,15 +8,28 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component("AOPTest")
+// 使用注解，定义切面
 @Aspect
+/**
+ * 通知（advice）:五种通知方式：
+ * @Before：前置通知，在调用目标方法之前执行通知定义的任务
+ * @After：后置通知，在目标方法执行结束后，无论执行结果如何都执行通知定义的任务
+ * @After-returning：后置通知，在目标方法执行结束后，如果执行成功，则执行通知定义的任务
+ * @After-throwing：异常通知，如果目标方法执行过程中抛出异常，则执行通知定义的任务
+ * @Around：环绕通知，在目标方法执行前和执行后，都需要执行通知定义的任务。
+ */
 public class AOPTest {
-    //定义切点
-    @Pointcut("execution(* *.saying(..))")
+    //使用注解，定义切点，尝试多种切点写法，都可用
+    //@Pointcut("execution(* *.saying(..))")
+    //@Pointcut("execution(* SayHello.saying(..))")
+    //@Pointcut("execution(* SayHello.saying())")
+    @Pointcut("execution(* wrx.demo.SayHello.saying())")
     public void sayings(){
     }
 
     /**
      * 前置通知
+     * 注意@Before注解的这个值"sayings()"，就是前面 @Pointcut切点所注解的方法
      */
     @Before("sayings()")
     public void sayHello(){
